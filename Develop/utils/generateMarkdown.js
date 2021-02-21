69815a254a85cfdf4795474f288fa73218c7d36a
jojoford <75343703+jojoford@users.noreply.github.com>
2021-02-21 11:40:57 -0600

    Update generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,26 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+//  A function that returns a license badge based on which license is passed in
+
+const addBadge = licenseChoice => {
+  if (licenseChoice == 'Apache License 2.0') {
+    return `
+![apache](https://img.shields.io/badge/license-Apache%20License%202.0-blue)
+    `;
+  } else if (licenseChoice == 'GNU GPLv3') {
+    return `
+![gnu](https://img.shields.io/badge/license-GNU%20GPLv3-green)
+    `;
+  } else if (licenseChoice == 'ISC License') {
+    return `
+![isc](https://img.shields.io/badge/license-ISC%20License-purple)
+    `;
+  } else {
+    return `
+![mit](https://img.shields.io/badge/license-MIT%20License-red)
+    `;
+  }
+}
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -10,10 +30,33 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${addBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${checkLicense(data.license)}
+## Contributing
+${data.contribution}
+## Tests
+${data.test}
+## Questions
+[GitHub Profile](https://github.com/${data.userName}/)  
+I can be reached at ${data.email}.
 `;
 }
 
