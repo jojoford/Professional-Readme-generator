b6423ec232870377dae9deb6f77d99cf2e0d5d57
jojoford <75343703+jojoford@users.noreply.github.com>
2021-02-21 12:54:05 -0600

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -18,102 +18,93 @@ const questions = [{
         }
     },
     {
-        type: 'input',
-        name: 'description',
-        message: 'Enter the project description: (REQUIRED)',
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('Please enter a project description!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter the installation instructions: (REQUIRED)',
-        validate: installationInput => {
-            if (installationInput) {
-                return true;
-            } else {
-                console.log('Please enter installation instructions!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage information: (REQUIRED)',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please enter usage information!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Enter contribution guidelines: (REQUIRED)',
-        validate: contributionInput => {
-            if (contributionInput) {
-                return true;
-            } else {
-                console.log('Please enter contribution information!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Enter test instructions: (REQUIRED)',
-        validate: testInput => {
-            if (testInput) {
-                return true;
-            } else {
-                console.log('Please enter test instructions!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Please select a license:',
-        choices: ['Apache License 2.0', 'GNU GPLv3', 'ISC License', 'MIT License']
-    },
-    {
-        type: 'input',
-        name: 'userName',
-        message: 'Enter your GitHub username: (REQUIRED)',
-        validate: userNameInput => {
-            if (userNameInput) {
-                return true;
-            } else {
-                console.log('Please enter a GitHub username!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address: (REQUIRED)',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('Please enter an email address!');
-                return false;
-            }
-        }
-    },
+    name: "os",
+    type: "list",
+    message: `This application will guide you through generating a README document.
+    Markdown formatting is supported.
+    Before we proceed, are you using a Windows machine or a Mac/Linux machine?`,
+    choices: ["Windows", "Mac/Linux"]
+  },
+  {
+    name: "Windows",
+    type: "confirm",
+    message: `The application will use Notepad for questions that may require longer responses. 
+    Are you ready to get started?`,
+    when: answers => answers.os === "Windows"
+  },
+  {
+    name: "Mac/Linux",
+    type: "confirm",
+    message: `The application will use Vim for questions that may require longer responses. 
+    To use Vim, press "enter" to open the editor and "i" to start typing.
+    When you're finished, press "esc" followed by ":wq" to save your answer and close Vim.
+    Are you ready to get started?`,
+    when: answers => answers.os === "Mac/Linux"
+  },
+  {
+    name: "name",
+    type: "input",
+    message: `Name:`
+  },
+  {
+    name: "email",
+    type: "input",
+    message: `Email Address:`
+  },
+  {
+    name: "github",
+    type: "input",
+    message: `GitHub Username:`
+  },
+  {
+    name: "description",
+    type: "editor",
+    message: `Describe the application:
+    (Markdown is supported in the editor)`
+  },
+  {
+    name: "tech",
+    type: "checkbox",
+    choices: ["HTML", "CSS", "Bootstrap 4", "JavaScript", "jQuery", "React.js", "Node.js", "Express", "MySQL", "MongoDb"],
+    message: "Select all technologies used in building the application:"
+  },
+  {
+    name: "installation",
+    type: "editor",
+    message: `How would a user install the application?
+    (Markdown is supported in the editor):`
+  },
+  {
+    name: "usage",
+    type: "editor",
+    message: `Please provide instructions for how to use the application:
+    (Markdown is supported in the editor)`
+  },
+  {
+    name: "contributing",
+    type: "editor",
+    message: `What are the guidelines for contributions to the project?
+    (Markdown is supported in the editor)`
+  },
+  {
+    name: "tests",
+    type: "editor",
+    message: `Describe the application's testing suites:
+    (Markdown is supported in the editor)`
+  },
+  {
+    name: "license",
+    type: "list",
+    choices: [ "Apache 2.0", "GPL 3.0", "GPL 2.0", "BSD 3 Clause", "None"],
+    message: "Select a license for your application:"
+  },
+  {
+    name: "badgeColor",
+    type: "list",
+    choices: ["brightgreen", "yellow", "red", "blue", "orange", "lightgray", "blueviolet"],
+    message: "Select a color for your license badge:",
+    when: answers => answers.license !== "None"
+  }
 ];
 
 const promptUser = () => {
